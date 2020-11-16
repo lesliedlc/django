bd1dfa273c355f975def29ba709aa93ffaeb654f
Posada De La Cruz <Leslie_PosadaDeLaCr1@baylor.edu>
2020-11-16 17:01:56 -0600

    incommit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,6 +6,7 @@ app_name = 'learning_logs'
 #'' null is the homepage
 ## 1 set up URL
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('', views.topics, name = 'topics') ##exercise
+    path('', views.index, name = 'index'), #'' is the homepage
+    path('topics', views.topics, name = 'topics'), #this would be homepage/topics/html (1)
+    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic #import from models.py file
 
 # Create your views here.
 # Defined as functions
@@ -6,6 +7,19 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'learning_logs/index.html')
 
-##exercise, aside from topics.html and base.html edits
-def topics(request):
-    return render(request, 'learning_logs/topics.html')
+def topics(request): #(2) get all topics
+    topics = Topic.objects.order_by('date_added')
+
+    context = {'topics':topics} #allows to use template variables
+
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id): #integer made in url - get individual topic
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added') # - is decending order
+
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request, 'learning_logs/topic.html', context) 
+
+
